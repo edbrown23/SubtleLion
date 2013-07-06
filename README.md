878d366b289c6cac881b49fffb0ed305afd491ec
Eric Brown <eric.d.brown23@gmail.com>
2013-07-06 17:12:44 -0400

    More project files

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-SubtleLion
+com.perceptron.SubtleLion.SubtleLion
 ==========
---
+++ SubtleLion-android/gen/com/perceptron/subtlelion/BuildConfig.java
@@ -0,0 +1,8 @@
+/*___Generated_by_IDEA___*/
+
+/** Automatically generated file. DO NOT MODIFY */
+package com.perceptron.subtlelion;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
